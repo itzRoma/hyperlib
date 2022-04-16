36d53a746549d494d78887ae8702c636f84bd858
itzRoma <romabondarenko03@gmail.com>
2022-04-16 22:53:23 +0300

    fixed order of categories in table

--- src/main/java/my/projects/hyperlib/repository/CategoryRepository.java
+++ src/main/java/my/projects/hyperlib/repository/CategoryRepository.java
@@ -8,12 +8,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
 
+    @Override
+    @Query(value = "SELECT * FROM categories ORDER BY id", nativeQuery = true)
+    List<Category> findAll();
+
     @Query(value = "SELECT * FROM categories INNER JOIN categories_item_types ON categories.id = categories_item_types.category_id WHERE categories_item_types.item_types = :item_type", nativeQuery = true)
-    Collection<Category> findByItemType(@Param("item_type") ItemType itemType);
+    List<Category> findByItemType(@Param("item_type") ItemType itemType);
 }
